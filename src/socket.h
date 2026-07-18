301aff01559a1cb449355857bac187634170fbc0
Sean Delaney <sdel@Mac.fritz.box>
2026-07-18 12:57:53 +0100

    network: make socket-worker shutdown explicit and reliable

--- src/socket.h
+++ src/socket.h
@@ -49,6 +49,7 @@
 #include <QObject>
 #include <QThread>
 #include <QMutex>
+#include <atomic>
 #include <vector>
 #include "global.h"
 #include "protocol.h"
@@ -176,14 +177,16 @@ public:
         Init();
     }
 
-    virtual ~CHighPrioSocket() { NetworkWorkerThread.Stop(); }
+    virtual ~CHighPrioSocket() { Stop(); }
 
     void Start()
     {
         // starts the high priority socket receive thread (with using blocking
         // socket request call)
-        NetworkWorkerThread.start ( QThread::TimeCriticalPriority );
+        NetworkWorkerThread.Start();
     }
+
+    void Stop() { NetworkWorkerThread.Stop(); }
 
     void SendPacket ( const CVector<uint8_t>& vecbySendBuf, const CHostAddress& HostAddr ) { Socket.SendPacket ( vecbySendBuf, HostAddr ); }
     void SendPacket ( const uint8_t* pbySendBuf, const int iNumBytes, const CHostAddress& HostAddr )
@@ -202,16 +205,36 @@ protected:
             setObjectName ( "CSocketThread" );
         }
 
+        void Start()
+        {
+            bFinished.store ( false, std::memory_order_relaxed );
+            bStarted.store ( true, std::memory_order_release );
+            start ( QThread::TimeCriticalPriority );
+        }
+
         void Stop()
         {
-            // disable run flag so that the thread loop can be exit
-            bRun = false;
+            // Publish the stop request once. Repeated Stop() calls still join
+            // the worker, but do not need to close the socket again.
+            if ( !bRun.exchange ( false, std::memory_order_acq_rel ) )
+            {
+                wait();
+                return;
+            }
 
             // to leave blocking wait for receive
             pSocket->Close();
 
-            // give thread some time to terminate
-            wait ( 5000 );
+            // Close() unblocks receive; wait without a timeout because owners
+            // may destroy state shared with the worker immediately afterward.
+            wait();
+            // Apple TSan does not model QThread::wait() as a join, so retain
+            // an explicit completion handoff for the worker's lifetime edge.
+            if ( bStarted.load ( std::memory_order_acquire ) )
+            {
+                while ( !bFinished.load ( std::memory_order_acquire ) )
+                    QThread::yieldCurrentThread();
+            }
         }
 
         void SetSocket ( CSocket* pNewSocket ) { pSocket = pNewSocket; }
@@ -223,17 +246,20 @@ protected:
             // case)
             if ( pSocket != nullptr )
             {
-                while ( bRun )
+                while ( bRun.load ( std::memory_order_acquire ) )
                 {
                     // this function is a blocking function (waiting for network
                     // packets to be received and processed)
                     pSocket->OnDataReceived();
                 }
             }
+            bFinished.store ( true, std::memory_order_release );
         }
 
-        CSocket* pSocket;
-        bool     bRun;
+        CSocket*          pSocket;
+        std::atomic<bool> bRun;
+        std::atomic<bool> bStarted{ false };
+        std::atomic<bool> bFinished{ true };
     };
 
     void Init()
